3e929691025acdae002144f71cbfe09d78dfa9b1
Anderson Conterno <conternoanderson@gmail.com>
2026-05-14 17:15:01 -0300

    fixando hhtps

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,11 +2,8 @@
 
 namespace App\Providers;
 
-use Carbon\CarbonImmutable;
-use Illuminate\Support\Facades\Date;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Validation\Rules\Password;
+use Illuminate\Support\Facades\URL; // <-- ADICIONE ESTA LINHA
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,28 +20,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        $this->configureDefaults();
-    }
-
-    /**
-     * Configure default behaviors for production-ready applications.
-     */
-    protected function configureDefaults(): void
-    {
-        Date::use(CarbonImmutable::class);
-
-        DB::prohibitDestructiveCommands(
-            app()->isProduction(),
-        );
-
-        Password::defaults(fn (): ?Password => app()->isProduction()
-            ? Password::min(12)
-                ->mixedCase()
-                ->letters()
-                ->numbers()
-                ->symbols()
-                ->uncompromised()
-            : null,
-        );
+        // Força HTTPS se o ambiente for produção (como na Render)
+        if (config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
     }
 }
